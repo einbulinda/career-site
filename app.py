6f2c789379ccf9e6d15252a90d3b680aabcbcaf5
Einstein Bulinda <einbulinda@gmail.com>
2023-02-23 09:10:29 +0300

    Adds dynamic data

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 # Creates a flask application
 app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Nairobi Kenya',
+        'salary': 400_000
+    },
+    {
+        'id': 2,
+        'title': 'Software Developer',
+        'location': 'Nairobi Kenya',
+        'salary': 240_000
+    },
+    {
+        'id': 3,
+        'title': 'UX Designer',
+        'location': 'Nairobi Kenya',
+        'salary': 140_000
+    },
+]
 
 
 # Register routes
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    # Pass the JOBs list to the template as an argument
+    return render_template('home.html', jobs=JOBS)
+
+
+@app.route('/api/jobs') #API Route
+def jobs_api():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':
